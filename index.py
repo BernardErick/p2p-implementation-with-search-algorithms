ad1ccf110cd1f6887de2530affd5a4b41ec1b3a9
Erick Bernardo <bernard.programmer@gmail.com>
2023-12-01 19:32:12 -0300

    cached

--- index.py
+++ index.py
@@ -4,11 +4,13 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import os
 import imageio
+import re
+import time
 
 def create_gif_from_pngs(directory="."):
     # Obtém a lista de arquivos PNG na raiz do projeto com o prefixo "graph"
     file_list = [f for f in os.listdir(directory) if f.endswith(".png") and f.startswith("graph")]
-    file_list.sort()  # Ordena a lista para garantir a ordem correta
+    file_list.sort(key=lambda x: int(re.search(r'\d+', x).group()) if re.search(r'\d+', x) else float('inf'))
 
     # Caminho para o arquivo de saída GIF
     output_gif_path = os.path.join(directory, "resume.gif")
@@ -18,27 +20,27 @@ def create_gif_from_pngs(directory="."):
 
     # Lê cada arquivo PNG e adiciona ao GIF
     for file_name in file_list:
-        print("adicionando ao gif")
+        print("Adicionando ao gif:", file_name)
         file_path = os.path.join(directory, file_name)
         images.append(imageio.imread(file_path))
 
-    # Salva o GIF com um intervalo de 1 segundo entre os frames
-    imageio.mimsave(output_gif_path, images, duration=400, loop=0)
+    # Salva o GIF com um intervalo de pelo menos 1 segundo entre os frames
+    imageio.mimsave(output_gif_path, images, duration=2000, loop=0)
 
     png_files = [f for f in os.listdir(directory) if f.endswith(".png") and f.startswith("graph")]
-
     # Exclui cada arquivo PNG
     for png_file in png_files:
         file_path = os.path.join(directory, png_file)
         os.remove(file_path)
-
+    
 
 class P2PNetwork:
-    def __init__(self, num_nodes, min_neighbors, max_neighbors):
+    def __init__(self, num_nodes, min_neighbors, max_neighbors, cache_path):
         self.num_nodes = num_nodes
         self.min_neighbors = min_neighbors
         self.max_neighbors = max_neighbors
         self.graph = nx.Graph()
+        self.cache_path = cache_path
 
     def print_graph(self):
         print("Graph:")
@@ -54,20 +56,21 @@ class P2PNetwork:
 
     def print_graph_with_interface(self):
         seed = 42
-        pos = nx.spring_layout(self.graph, seed=seed)  # Layout para a visualização do grafo
+        pos = nx.spring_layout(self.graph, seed=seed, k=1.5)  # Layout para a visualização do grafo
         node_labels = {node: f"{node}\n{', '.join(self.graph.nodes[node]['resources'])}" for node in self.graph.nodes}
 
+        plt.figure(figsize=(13, 10))
         nx.draw(self.graph, pos, with_labels=True, labels=node_labels, font_weight='bold', node_color='skyblue')
         plt.savefig("graph.png")
         
     def print_graph_with_interface_advanced(self, visited_nodes=None, current_node=None):
         plt.figure()
         seed = 42
-        pos = nx.spring_layout(self.graph, seed=seed)  # Layout para a visualização do grafo
+        pos = nx.spring_layout(self.graph, seed=seed, k=1.5)  # Layout para a visualização do grafo
         node_labels = {node: f"{node}\n{', '.join(self.graph.nodes[node]['resources'])}" for node in self.graph.nodes}
         
         node_colors = ['green' if node == current_node else 'red' if node in visited_nodes else 'skyblue' for node in self.graph.nodes]
-
+        plt.figure(figsize=(13, 10))
         nx.draw(self.graph, pos, with_labels=True, labels=node_labels, font_weight='bold', node_color=node_colors)
         value = len(visited_nodes)
         
@@ -94,24 +97,6 @@ class P2PNetwork:
             if edge[0] == edge[1]:
                 raise ValueError("Existem arestas de um nó para ele mesmo.")
 
-    def search_resources(self, node_id, resource_id, ttl, algo):
-        if algo == 'flooding':
-            messages, involved_nodes, history = self.flooding_search(node_id, resource_id, ttl)
-        elif algo == 'informed_flooding':
-            messages, involved_nodes, history = self.informed_flooding_search(node_id, resource_id, ttl)
-        elif algo == 'random_walk':
-            messages, involved_nodes, history = self.random_walk_search(node_id, resource_id, ttl)
-        elif algo == 'informed_random_walk':
-            messages, involved_nodes, history = self.informed_random_walk_search(node_id, resource_id, ttl)
-        else:
-            raise ValueError("Algoritmo de busca inválido.")
-
-        print(f"Número total de mensagens: {messages}")
-        print(f"Número total de nós envolvidos: {involved_nodes}")
-        print("\nHistórico:")
-        for step in history:
-            print(step)
-
     #Busca por Inundação
     def flooding_search(self, node_id, resource_id, ttl):
         visited = set()
@@ -120,6 +105,7 @@ class P2PNetwork:
         involved_nodes = set()
         history = []
 
+        start_time = time.time()
         while queue:
             current_node, current_ttl = queue.pop(0)
             
@@ -132,6 +118,9 @@ class P2PNetwork:
                 self.print_graph_with_interface_advanced(visited, current_node)
                 if resource_id in self.graph.nodes[current_node]['resources']:
                     history.append(f"Encontrado em {current_node}!")
+                    end_time = time.time()
+                    elapsed_time = end_time - start_time
+                    print(f"Tempo decorrido do algitmo de busca por inundação SEM CACHE: {elapsed_time} segundos")
                     return messages, len(involved_nodes), history
                 else:
                     history.append(f"Não encontrado!")
@@ -145,15 +134,104 @@ class P2PNetwork:
                             queue.append((neighbor, current_ttl - 1))
                             messages += 1
                             history.append(f"{current_node} perguntou para {neighbor} sobre o recurso {resource_id}")
-        
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Tempo decorrido do algitmo de busca por inundação SEM CACHE: {elapsed_time} segundos")
         return messages, len(involved_nodes), history
 
     def informed_flooding_search(self, node_id, resource_id, ttl):
-        # Implementar busca informada por inundação
-        print("informed_flooding_search")
-        return "Nada","ainda"
-        pass
+        start_time = time.time()
 
+        visited = set()
+        queue = [(node_id, ttl)]
+        messages = 0
+        involved_nodes = set()
+        history = []
+
+        cached_result = self.get_cache_value(resource_id)
+        if cached_result is not None:
+            print(f"Recurso encontrado no cache! Valor: {cached_result}")
+
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"Tempo decorrido do algitmo de busca por inundação COM CACHE: {elapsed_time} segundos")
+            return messages, len(involved_nodes), history
+
+        while queue:
+            current_node, current_ttl = queue.pop(0)
+            
+            involved_nodes.add(current_node)
+
+            history.append(f"{current_node} verificou nele mesmo ({current_node}) sobre o recurso {resource_id}")
+
+            if current_node not in visited:
+                visited.add(current_node)
+                self.print_graph_with_interface_advanced(visited, current_node)
+                if resource_id in self.graph.nodes[current_node]['resources']:
+                    
+                    # Use o caminho absoluto para o arquivo de cache
+                    self.save_cache(str(resource_id), str(current_node))
+                    
+                    history.append(f"Encontrado em {current_node}!")
+                    end_time = time.time()
+                    elapsed_time = end_time - start_time
+                    print(f"Tempo decorrido do algitmo de busca por inundação COM CACHE: {elapsed_time} segundos")
+                    return messages, len(involved_nodes), history
+                else:
+                    history.append(f"Não encontrado!")
+
+                if current_ttl > 0:
+                    neighbors = list(self.graph.neighbors(current_node))
+                    for neighbor in neighbors:
+                        if neighbor not in visited:
+                            print("Comunicação de vizinhos do ",current_node, " -> ", neighbor)
+                            #TODO não incluir vizinhos já visitados
+                            queue.append((neighbor, current_ttl - 1))
+                            messages += 1
+                            history.append(f"{current_node} perguntou para {neighbor} sobre o recurso {resource_id}")
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Tempo decorrido do algitmo de busca por inundação COM CACHE: {elapsed_time} segundos")
+        return messages, len(involved_nodes), history
+
+    def save_cache(self, key, value):
+        print("Chave:",key)
+        print("Chave:",value)
+        cache_file = self.cache_path
+
+        # Adicione mensagens de debug
+        print(f"Debug: Caminho absoluto para o arquivo de cache: {cache_file}")
+        # Verifica se o arquivo de cache já existe
+        if os.path.exists(cache_file):
+            # Carrega o conteúdo atual do arquivo JSON
+            with open(cache_file, 'r') as file:
+                cache_data = json.load(file)
+        else:
+            # Se o arquivo não existir, cria um dicionário vazio
+            cache_data = {}
+
+        # Adiciona a chave e valor ao dicionário
+        cache_data[key] = value
+
+        # Salva o dicionário atualizado de volta no arquivo JSON
+        with open(cache_file, 'w') as file:
+            json.dump(cache_data, file, indent=2)
+    def get_cache_value(self,key):
+        cache_file = "cache.json"
+
+        # Verifica se o arquivo de cache existe
+        if os.path.exists(cache_file):
+            # Carrega o conteúdo atual do arquivo JSON
+            with open(cache_file, 'r') as file:
+                cache_data = json.load(file)
+
+            # Verifica se a chave existe no dicionário
+            if key in cache_data:
+                # Retorna o valor correspondente à chave
+                return cache_data[key]
+
+    # Retorna None se a chave não existir ou o arquivo não existir
+    
     def random_walk_search(self, node_id, resource_id, ttl):
         # Implementar busca por passeio aleatório
         print("random_walk_search")
@@ -165,6 +243,7 @@ class P2PNetwork:
         print("informed_random_walk_search")
         return "Nada","ainda"
         pass
+
 
 def parse_config_from_file(file_path):
     with open(file_path, 'r') as file:
@@ -182,7 +261,7 @@ def parse_config(config_data):
     print(min_neighbors)
     print(max_neighbors)
 
-    network = P2PNetwork(num_nodes, min_neighbors, max_neighbors)
+    network = P2PNetwork(num_nodes, min_neighbors, max_neighbors, '.')
     
     for node_resources in config["resources"]:
         node_id, resources = node_resources.split(":")
@@ -197,17 +276,20 @@ def parse_config(config_data):
 
 
 if __name__ == "__main__":
-
-    # Caminho para o arquivo bootstrap.json na raiz do projeto
+ # Caminho para o arquivo bootstrap.json na raiz do projeto
     bootstrap_file_path = os.path.join(os.getcwd(), "bootstrap.json")
+
+    # Caminho absoluto para o arquivo de cache
+    cache_file_path = os.path.join(os.getcwd(), "cache.json")
 
     # Verifica se o arquivo bootstrap.json existe
     if not os.path.exists(bootstrap_file_path):
         print("Arquivo bootstrap.json não encontrado na raiz do projeto.")
         exit(1)
 
-    # Lê o conteúdo do arquivo bootstrap.json
+    # Lê o conteúdo do arquivo bootstrap.json e instância a classe P2PNetwork com o caminho absoluto para o arquivo de cache
     network = parse_config_from_file(bootstrap_file_path)
+    network.cache_path = cache_file_path
     
     try:
         network.validate_network()
@@ -221,10 +303,10 @@ if __name__ == "__main__":
     # Definindo qual o tipo de teste eu vou querer
     node_id = 'n1'
     resource_id = 'r11'
-    ttl = 4
-    algo = 'flooding'
+    ttl = 30
+    algo = 'informed_flooding'
 
-    messages, involved_nodes, history = network.flooding_search(node_id, resource_id, ttl)
+    messages, involved_nodes, history = network.informed_flooding_search(node_id, resource_id, ttl)
     
     print(f"\nResultado da busca por inundação:")
     print(f"Número total de mensagens: {messages}")
@@ -235,4 +317,3 @@ if __name__ == "__main__":
         print(step)
 
     create_gif_from_pngs()
-    
