254feff90bd667b7b932203fb4efa0ac397bf005
Bernard Erick <49220123+BernardErick@users.noreply.github.com>
2023-12-01 14:14:12 -0300

    Update index.py

--- index.py
+++ index.py
@@ -2,9 +2,31 @@ import json
 import random
 import networkx as nx
 import matplotlib.pyplot as plt
+import os
+import imageio
+
+def create_gif_from_pngs(directory="."):
+    # Obtém a lista de arquivos PNG na raiz do projeto com o prefixo "graph"
+    file_list = [f for f in os.listdir(directory) if f.endswith(".png") and f.startswith("graph")]
+    file_list.sort()  # Ordena a lista para garantir a ordem correta
+
+    # Caminho para o arquivo de saída GIF
+    output_gif_path = os.path.join(directory, "resume.gif")
+
+    # Lista para armazenar os frames do GIF
+    images = []
+
+    # Lê cada arquivo PNG e adiciona ao GIF
+    for file_name in file_list:
+        print("adicionando ao gif")
+        file_path = os.path.join(directory, file_name)
+        images.append(imageio.imread(file_path))
+
+    # Salva o GIF com um intervalo de 1 segundo entre os frames
+    imageio.mimsave(output_gif_path, images, duration=500, loop=0)
 
 class P2PNetwork:
-    def __init__(self, num_nodes, min_neighbors, max_neighbors):
+    def _init_(self, num_nodes, min_neighbors, max_neighbors):
         self.num_nodes = num_nodes
         self.min_neighbors = min_neighbors
         self.max_neighbors = max_neighbors
@@ -23,11 +45,25 @@ class P2PNetwork:
         print("\n")
 
     def print_graph_with_interface(self):
-        pos = nx.spring_layout(self.graph)  # Layout para a visualização do grafo
+        seed = 42
+        pos = nx.spring_layout(self.graph, seed=seed)  # Layout para a visualização do grafo
         node_labels = {node: f"{node}\n{', '.join(self.graph.nodes[node]['resources'])}" for node in self.graph.nodes}
 
         nx.draw(self.graph, pos, with_labels=True, labels=node_labels, font_weight='bold', node_color='skyblue')
         plt.savefig("graph.png")
+        
+    def print_graph_with_interface_advanced(self, visited_nodes=None, current_node=None):
+        plt.figure()
+        seed = 42
+        pos = nx.spring_layout(self.graph, seed=seed)  # Layout para a visualização do grafo
+        node_labels = {node: f"{node}\n{', '.join(self.graph.nodes[node]['resources'])}" for node in self.graph.nodes}
+        
+        node_colors = ['green' if node == current_node else 'red' if node in visited_nodes else 'skyblue' for node in self.graph.nodes]
+
+        nx.draw(self.graph, pos, with_labels=True, labels=node_labels, font_weight='bold', node_color=node_colors)
+        value = len(visited_nodes)
+        
+        plt.savefig("graph" + str(value) + ".png")
 
     def validate_network(self):
         # Verificação 1: A rede não pode estar particionada
@@ -78,25 +114,30 @@ class P2PNetwork:
 
         while queue:
             current_node, current_ttl = queue.pop(0)
-            messages += 1
+            
             involved_nodes.add(current_node)
 
             history.append(f"{current_node} verificou nele mesmo ({current_node}) sobre o recurso {resource_id}")
 
             if current_node not in visited:
                 visited.add(current_node)
+                self.print_graph_with_interface_advanced(visited, current_node)
                 if resource_id in self.graph.nodes[current_node]['resources']:
                     history.append(f"Encontrado em {current_node}!")
                     return messages, len(involved_nodes), history
+                else:
+                    history.append(f"Não encontrado!")
 
                 if current_ttl > 0:
                     neighbors = list(self.graph.neighbors(current_node))
                     for neighbor in neighbors:
-                        print("Comunicação de vizinhos do ",current_node, " -> ", neighbor)
-                        #TODO não incluir vizinhos já visitados
-                        queue.append((neighbor, current_ttl - 1))
-                        history.append(f"{current_node} perguntou para {neighbor} sobre o recurso {resource_id}")
-
+                        if neighbor not in visited:
+                            print("Comunicação de vizinhos do ",current_node, " -> ", neighbor)
+                            #TODO não incluir vizinhos já visitados
+                            queue.append((neighbor, current_ttl - 1))
+                            messages += 1
+                            history.append(f"{current_node} perguntou para {neighbor} sobre o recurso {resource_id}")
+        
         return messages, len(involved_nodes), history
 
     def informed_flooding_search(self, node_id, resource_id, ttl):
@@ -142,7 +183,7 @@ def parse_config(config_data):
 if __name__ == "__main__":
     config_data = '''
     {
-        "num_nodes": 6,
+        "num_nodes": 10,
         "min_neighbors": 1,
         "max_neighbors": 4,
         "resources" :[
@@ -153,7 +194,9 @@ if __name__ == "__main__":
             "n5: r9, r10",
             "n6: r11, r12",
             "n7: r13, r14",
-            "n8: r15, r16"
+            "n8: r15, r16",
+            "n9: r17, r18",
+            "n10: r21, r20"
         ],
         "edges" :[
             "n1, n2",
@@ -162,11 +205,12 @@ if __name__ == "__main__":
             "n4, n5",
             "n5, n6",
             "n3, n7",
-            "n3, n8"
+            "n3, n8",
+            "n4, n9",
+            "n4, n10"
         ]
     }
     '''
-
     network = parse_config(config_data)
 
     try:
@@ -180,8 +224,8 @@ if __name__ == "__main__":
 
     # Exemplo de uso da busca por inundação
     node_id = 'n1'
-    resource_id = 'r7'
-    ttl = 8
+    resource_id = 'r11'
+    ttl = 4
     algo = 'flooding'
 
     messages, involved_nodes, history = network.flooding_search(node_id, resource_id, ttl)
@@ -193,3 +237,5 @@ if __name__ == "__main__":
 
     for step in history:
         print(step)
+
+    create_gif_from_pngs()
